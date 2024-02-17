3ec5e72a67cd4a94d5d361f92ce89a881d69bcaa
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-02-17 22:48:11 +0100

    DB moved to persoanl for deployment

--- db/index.js
+++ db/index.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
-const MONGO_URI = `mongodb+srv://admin:${process.env.MONGO_PASS}@cluster0.imrhryg.mongodb.net/`;
+const password = process.env.MONGO_PASSWORD
+const MONGO_URI = `mongodb+srv://user:${password}@cluster0.ztdoc7q.mongodb.net/`;
 
 mongoose
   .connect(MONGO_URI)
